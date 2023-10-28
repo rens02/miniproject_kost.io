71ec2ceb3615ee3798cdeec0ff495aeb8e0938f6
rens02 <cyberlearninghack@gmail.com>
2023-10-28 14:06:07 +0700

    enhc(RentStatus): add rent status const

--- models/rent.go
+++ models/rent.go
@@ -3,16 +3,22 @@ package models
 import "gorm.io/gorm"
 
 type RoomStatus string
+type RentStatus string
 
 const (
 	RoomStatusAvailable RoomStatus = "Available"
 	RoomStatusOccupied  RoomStatus = "Occupied"
+)
+const (
+	RentStatusBooked   RentStatus = "Booked"
+	RentStatusCanceled RentStatus = "Canceled"
 )
 
 type Sewa struct {
 	gorm.Model
 	AvailableID   uint          `json:"RoomAvailableID"`
 	UserID        uint          `json:"UserID"`
+	RentStatus    RentStatus    `gorm:"type:enum('Booked', 'Canceled')" json:"rent_status"`
 	User          User          `gorm:"foreignKey:UserID" json:"user"`
 	KamarTersedia KamarTersedia `gorm:"foreignKey:AvailableID" json:"kamar_tersedia"`
 }
